9a621e2cef3d2de96620159d6094f4bd8d5c82aa
Darrin <darrin@ms7.url.com.tw>
2020-11-29 17:24:33 +0800

    update procedures.js to add parameters

--- ArdublocklySketch/ArdublocklySketch/ArdublocklySketch.ino
+++ ArdublocklySketch/ArdublocklySketch/ArdublocklySketch.ino
@@ -1,5 +1,14 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C I2CLCD( 0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
 void setup() {
+  I2CLCD.begin(16,2);
 }
 
 void loop() {
+  I2CLCD.setCursor(0, 0);
+  I2CLCD.print("Hello World");
+
 }
