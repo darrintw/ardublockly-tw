caee742a4ed1c1e76a2571f02801c7ae82e72f5d
Darrin <darrin@ms7.url.com.tw>
2021-05-11 15:51:12 +0800

    fix update command

--- update.bat
+++ update.bat
@@ -6,9 +6,9 @@ echo ***************************Ardublockly Update Tool*************************
 @echo off
 
 echo.
-IF EXIST "%~dp0\Ardublockly\.git\index.lock" (
-	del /f /s /q "%~dp0\Ardublockly\.git\index.lock" > nul
-	rd /q /s "%~dp0\Ardublockly\.git\index.lock" > nul
+IF EXIST "%~dp0\.git\index.lock" (
+	del /f /s /q "%~dp0\.git\index.lock" > nul
+	rd /q /s "%~dp0\.git\index.lock" > nul
 )
 @echo off
 
@@ -16,80 +16,80 @@ cd "%~dp0"
 rem update blocks
 ..\PortableGit\cmd\git config --system core.longpaths true
 ..\PortableGit\cmd\git reset --hard origin/master
-..\PortableGit\cmd\git pull origin master
+..\PortableGit\cmd\git pull --force origin master
 
 ..\PortableGit\cmd\git gc
 ..\PortableGit\cmd\git prune
 
-IF EXIST "%~dp0\Ardublockly\__pycache__" (
+IF EXIST "%~dp0\__pycache__" (
 	echo.
-	del /f /s /q "%~dp0\Ardublockly\__pycache__" > nul
-	rd /q /s "%~dp0\Ardublockly\__pycache__" > nul
+	del /f /s /q "%~dp0\__pycache__" > nul
+	rd /q /s "%~dp0\__pycache__" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0\Ardublockly\notepad++" (
+IF EXIST "%~dp0\notepad++" (
 	echo.
-	del /f /s /q "%~dp0\Ardublockly\notepad++" > nul
-	rd /q /s "%~dp0\Ardublockly\notepad++" > nul
+	del /f /s /q "%~dp0\notepad++" > nul
+	rd /q /s "%~dp0\notepad++" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0\Ardublockly\package" (
+IF EXIST "%~dp0\package" (
 	echo.
-	del /f /s /q "%~dp0\Ardublockly\package" > nul
-	rd /q /s "%~dp0\Ardublockly\package" > nul
+	del /f /s /q "%~dp0\package" > nul
+	rd /q /s "%~dp0\package" > nul
 	@echo off
 )
 
 
-IF EXIST "%~dp0\Ardublockly\PortableGit" (
+IF EXIST "%~dp0\PortableGit" (
 	echo.
-	del /f /s /q "%~dp0\Ardublockly\PortableGit" > nul
-	rd /q /s "%~dp0\Ardublockly\PortableGit" > nul
+	del /f /s /q "%~dp0\PortableGit" > nul
+	rd /q /s "%~dp0\PortableGit" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0\Ardublockly\DigisparkWindowsDriver" (
+IF EXIST "%~dp0\DigisparkWindowsDriver" (
 	echo.
-	del /f /s /q "%~dp0\Ardublockly\DigisparkWindowsDriver" > nul
-	rd /q /s "%~dp0\Ardublockly\DigisparkWindowsDriver" > nul
+	del /f /s /q "%~dp0\DigisparkWindowsDriver" > nul
+	rd /q /s "%~dp0\DigisparkWindowsDriver" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0\Ardublockly\ardublockly.iml" (
+IF EXIST "%~dp0\ardublockly.iml" (
 	echo.
-	del /f /s /q "%~dp0\Ardublockly\ardublockly.iml" > nul
+	del /f /s /q "%~dp0\\ardublockly.iml" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0\Ardublockly\ardublockly.log" (
+IF EXIST "%~dp0\ardublockly.log" (
 	echo.
-	del /f /s /q "%~dp0\Ardublockly\ardublockly.log" > nul
+	del /f /s /q "%~dp0\ardublockly.log" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0\Ardublockly\build.bat" (
+IF EXIST "%~dp0\build.bat" (
 	echo.
-	del /f /s /q "%~dp0\Ardublockly\build.bat" > nul
+	del /f /s /q "%~dp0\build.bat" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0\Ardublockly\pack.bat" (
+IF EXIST "%~dp0\pack.bat" (
 	echo.
-	del /f /s /q "%~dp0\Ardublockly\pack.bat" > nul
+	del /f /s /q "%~dp0\pack.bat" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0\Ardublockly\run.bat" (
+IF EXIST "%~dp0\run.bat" (
 	echo.
-	del /f /s /q "%~dp0\Ardublockly\run_dev.bat" > nul
+	del /f /s /q "%~dp0\run_dev.bat" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0\Ardublockly\run_dev.bat" (
+IF EXIST "%~dp0\run_dev.bat" (
 	echo.
-	del /f /s /q "%~dp0\Ardublockly\run_dev.bat" > nul
+	del /f /s /q "%~dp0\run_dev.bat" > nul
 	@echo off
 )
 
