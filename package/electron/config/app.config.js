09a27718c715d95ffb9b758b83c8578e4ac55b95
Darrin <darrin@ms7.url.com.tw>
2021-05-11 20:13:16 +0800

    remove web non used files

--- package/electron/config/app.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  platform: process.platform,
-  port: process.env.PORT ? process.env.PORT : 3000,
-  title: 'PhraseApp Electron i18n',
-  languages: ['en', 'zh-hant'],
-  fallbackLng: 'zh-hant',
-  namespace: 'translation'
-};
