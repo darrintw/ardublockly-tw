0c1471c2844b484404eb0e5f7e2acf1e578ad3a4
Darrin <darrin@ms7.url.com.tw>
2021-05-11 08:44:45 +0800

    update gitignore, update sctipt and add CH341

--- update.bat
+++ update.bat
@@ -6,13 +6,13 @@ echo ***************************Ardublockly Update Tool*************************
 @echo off
 
 echo.
-IF EXIST "%~dp0"\Ardublockly\.git\index.lock (
-	del /f /s /q "%~dp0"\Ardublockly\.git\index.lock > nul
-	rd /q /s "%~dp0"\Ardublockly\.git\index.lock > nul
+IF EXIST "%~dp0\Ardublockly\.git\index.lock" (
+	del /f /s /q "%~dp0\Ardublockly\.git\index.lock" > nul
+	rd /q /s "%~dp0\Ardublockly\.git\index.lock" > nul
 )
 @echo off
 
-cd "%~dp0"\..\PortableGit\cmd\
+cd "%~dp0\..\PortableGit\cmd\"
 rem update blocks
 git config --system core.longpaths true
 git --git-dir=..\..\Ardublockly\.git\ reset --hard origin/master
@@ -21,69 +21,63 @@ git --git-dir=..\..\Ardublockly\.git\ pull origin master
 git --git-dir=..\..\Ardublockly\.git\ gc
 git --git-dir=..\..\Ardublockly\.git\ prune
 
-IF EXIST "%~dp0"\Ardublockly\__pycache__ (
+IF EXIST %~dp0\Ardublockly\__pycache__" (
 	echo.
-	del /f /s /q "%~dp0"\Ardublockly\__pycache__ > nul
-	rd /q /s "%~dp0"\Ardublockly\__pycache__ > nul
+	del /f /s /q "%~dp0\Ardublockly\__pycache__" > nul
+	rd /q /s "%~dp0\Ardublockly\__pycache__" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0"\Ardublockly\notepad++ (
+IF EXIST "%~dp0\Ardublockly\notepad++" (
 	echo.
-	del /f /s /q "%~dp0"\Ardublockly\notepad++ > nul
-	rd /q /s "%~dp0"\Ardublockly\notepad++ > nul
+	del /f /s /q "%~dp0\Ardublockly\notepad++" > nul
+	rd /q /s "%~dp0\Ardublockly\notepad++" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0"\Ardublockly\package (
+IF EXIST "%~dp0\Ardublockly\package" (
 	echo.
-	del /f /s /q "%~dp0"\Ardublockly\package > nul
-	rd /q /s "%~dp0"\Ardublockly\package > nul
+	del /f /s /q "%~dp0\Ardublockly\package" > nul
+	rd /q /s "%~dp0\Ardublockly\package" > nul
 	@echo off
 )
 
 
-IF EXIST "%~dp0"\Ardublockly\PortableGit (
+IF EXIST "%~dp0\Ardublockly\PortableGit" (
 	echo.
-	del /f /s /q "%~dp0"\Ardublockly\PortableGit > nul
-	rd /q /s "%~dp0"\Ardublockly\PortableGit > nul
+	del /f /s /q "%~dp0\Ardublockly\PortableGit" > nul
+	rd /q /s "%~dp0\Ardublockly\PortableGit" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0"\Ardublockly\DigisparkWindowsDriver (
+IF EXIST "%~dp0\Ardublockly\DigisparkWindowsDriver" (
 	echo.
-	del /f /s /q "%~dp0"\Ardublockly\DigisparkWindowsDriver > nul
-	rd /q /s "%~dp0"\Ardublockly\DigisparkWindowsDriver > nul
+	del /f /s /q "%~dp0\Ardublockly\DigisparkWindowsDriver" > nul
+	rd /q /s "%~dp0\Ardublockly\DigisparkWindowsDriver" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0"\Ardublockly\ardublockly.log (
+IF EXIST "%~dp0\Ardublockly\ardublockly.log" (
 	echo.
-	del /f /s /q "%~dp0"\Ardublockly\ardublockly.log > nul
+	del /f /s /q "%~dp0\Ardublockly\ardublockly.log" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0"\Ardublockly\build.bat (
+IF EXIST "%~dp0\Ardublockly\build.bat" (
 	echo.
-	del /f /s /q "%~dp0"\Ardublockly\build.bat > nul
+	del /f /s /q "%~dp0\Ardublockly\build.bat" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0"\Ardublockly\pack.bat (
+IF EXIST "%~dp0\Ardublockly\pack.bat" (
 	echo.
-	del /f /s /q "%~dp0"\Ardublockly\pack.bat > nul
+	del /f /s /q "%~dp0\Ardublockly\pack.bat" > nul
 	@echo off
 )
 
-IF EXIST "%~dp0"\Ardublockly\run_dev.bat (
+IF EXIST "%~dp0\Ardublockly\run_dev.bat" (
 	echo.
-	del /f /s /q "%~dp0"\Ardublockly\run_dev.bat > nul
-	@echo off
-)
-
-IF EXIST "%~dp0"\Ardublockly\update.bat (
-	echo.
-	del /f /s /q "%~dp0"\Ardublockly\update.bat > nul
+	del /f /s /q "%~dp0\Ardublockly\run_dev.bat" > nul
 	@echo off
 )
 
